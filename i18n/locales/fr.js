d4d83b04a0ef00584415281ff9458d7f8efb0295
oumasma <oumasma93@hotmail.fr>
2020-12-10 15:06:42 +0400

    translation done

--- i18n/locales/fr.js
+++ i18n/locales/fr.js
@@ -6,42 +6,42 @@ export default {
   },
   welcomepage: {
     title: 'Bienvenue',
-    choiceDatas: 'Before we get started, we value your prevacy and your data only belongs to you : where would you like to store it ?',
+    choiceDatas: 'Avant de commencer, nous préservons ctre vie privée et vos données n\'appartiennent qu\'à vous: où souhaitez-vous les stocker?',
     cloud: 'Cloud',
     phone: 'Télépone',
-    cloudSentence: 'Allows you to access your data from anywhere and even share it with your partner.',
-    phoneSentence: 'Keep your data on your phone (no cloud storage). Share or access data elsewhere disable.',
-    canChange: 'You can change it anytime from your control panel.'
+    cloudSentence: 'Vous permet d\'accéder à vos données de n\'importe où et même de les partager avec votre partenaire.',
+    phoneSentence: 'Gardez vos données sur votre téléphone (pas de stockage dans le cloud). Partager ou accéder aux données ailleurs désactiver.',
+    canChange: 'Vous pouvez le modifier à tout moment depuis votre panneau de contrôle.'
   },
   welcomepage2: {
     title: 'Bienvenue',
-    choiceDatas: 'We are happy to have you on board. Let\'s get to know eachother better: how would you like to use this app ?',
-    pregnancy: 'Pregnancy',
-    alternative: 'Alternative birth control',
-    aboutYourself: 'Know more about myself',
-    canChange: 'You can change it anytime from your control panel.'
+    choiceDatas: 'Nous sommes heureux de vous avoir à bord. Apprenons à mieux nous connaître: comment aimeriez-vous utiliser cette application?',
+    pregnancy: 'Grossesse',
+    alternative: 'Contrôle des naissances alternatif',
+    aboutYourself: 'En savoir plus sur moi',
+    canChange: 'Vous pouvez le modifier à tout moment depuis votre panneau de contrôle.'
   },
   beAware: {
     title: 'Avertissement',
-    warning: 'This app does not replace any medical advices. It is made to assist you in better understanding yourself. Please keep this app as a tool and not a source of truth.',
+    warning: 'Cette application ne remplace aucun avis médical. Il est fait pour vous aider à mieux vous comprendre. Veuillez garder cette application comme un outil et non comme une source de vérité.',
     understand: 'J\'ai compris',
     submit: 'Valider'
   },
   thermometer: {
     title: 'Thermomètre',
-    infoSentence: 'As symptothermy is based on daily temperature measurement, we need to make sure you have the right material to use this app.',
-    symptosensor: 'I have a Symptosensor',
-    decimalthermo: 'I have my own double decimal thermometer',
-    why: 'Why do I need that ?'
+    infoSentence: 'La symptothermie étant basée sur la mesure quotidienne de la température, nous devons nous assurer que vous disposez du bon matériel pour utiliser cette application.',
+    symptosensor: 'J\'ai un capteur',
+    decimalthermo: 'J\'ai mon propre thermomètre à double décimales',
+    why: 'Pourquoi ai-je besoin de ça ?'
   },
   yourProfil: {
     title: 'Profil',
-    infoDatas: 'These data are yours and it will only be used by the app itself to improve your user experience.',
-    alreadyUse: 'We are happy to know you are already using symptothermy in your daily life. Please add your previous infos below so we can take it into account.',
-    hormonalControl: 'Do you use an hormonal birth control ?',
-    hormonalStopped: 'For how long have you stopped using an hormonal birth control ?',
+    infoDatas: 'Ces données vous appartiennent et ne seront utilisées que par l\'application elle-même pour améliorer votre expérience utilisateur.',
+    alreadyUse: 'Nous sommes heureux de savoir que vous utilisez déjà la symptothermie dans votre vie quotidienne. Veuillez ajouter vos informations précédentes ci-dessous afin que nous puissions en tenir compte.',
+    hormonalControl: 'Utilisez-vous un contraceptif hormonal ?',
+    hormonalStopped: 'Depuis combien de temps avez-vous cessé d\'utiliser un contraceptif hormonal?',
     cycleaverageTime: 'Durée moyenne du cycle',
-    nubmerOfDays: 'Nombre de jour',
+    nubmerOfDays: 'Nombre de jours',
     nubmerOfCycles: 'Nombre de cycles',
     symptothermyExperience: 'Expérience en symptothermie',
     longestCycle: 'Cycle le plu long',
