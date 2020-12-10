3fa84cf4740660e78fa01a8865c4e547060d989f
oumasma <oumasma93@hotmail.fr>
2020-12-10 14:51:13 +0400

    add traslate texts

--- i18n/locales/fr.js
+++ i18n/locales/fr.js
@@ -4,4 +4,77 @@ export default {
     title: 'Symptothermie',
     welcome: 'Débloque ta propre magie',
   },
+  welcomepage: {
+    title: 'Bienvenue',
+    choiceDatas: 'Before we get started, we value your prevacy and your data only belongs to you : where would you like to store it ?',
+    cloud: 'Cloud',
+    phone: 'Télépone',
+    cloudSentence: 'Allows you to access your data from anywhere and even share it with your partner.',
+    phoneSentence: 'Keep your data on your phone (no cloud storage). Share or access data elsewhere disable.',
+    canChange: 'You can change it anytime from your control panel.'
+  },
+  welcomepage2: {
+    title: 'Bienvenue',
+    choiceDatas: 'We are happy to have you on board. Let\'s get to know eachother better: how would you like to use this app ?',
+    pregnancy: 'Pregnancy',
+    alternative: 'Alternative birth control',
+    aboutYourself: 'Know more about myself',
+    canChange: 'You can change it anytime from your control panel.'
+  },
+  beAware: {
+    title: 'Avertissement',
+    warning: 'This app does not replace any medical advices. It is made to assist you in better understanding yourself. Please keep this app as a tool and not a source of truth.',
+    understand: 'J\'ai compris',
+    submit: 'Valider'
+  },
+  thermometer: {
+    title: 'Thermomètre',
+    infoSentence: 'As symptothermy is based on daily temperature measurement, we need to make sure you have the right material to use this app.',
+    symptosensor: 'I have a Symptosensor',
+    decimalthermo: 'I have my own double decimal thermometer',
+    why: 'Why do I need that ?'
+  },
+  yourProfil: {
+    title: 'Profil',
+    infoDatas: 'These data are yours and it will only be used by the app itself to improve your user experience.',
+    alreadyUse: 'We are happy to know you are already using symptothermy in your daily life. Please add your previous infos below so we can take it into account.',
+    hormonalControl: 'Do you use an hormonal birth control ?',
+    hormonalStopped: 'For how long have you stopped using an hormonal birth control ?',
+    cycleaverageTime: 'Durée moyenne du cycle',
+    nubmerOfDays: 'Nombre de jour',
+    nubmerOfCycles: 'Nombre de cycles',
+    symptothermyExperience: 'Expérience en symptothermie',
+    longestCycle: 'Cycle le plu long',
+    shortestCycle: 'Cycle le plus court',
+    earliestTemperature: 'Température la plus haute dans le cycle',
+    cycleDay: 'Jour du cycle',
+    neverUsed: 'Jamais utilisé d\'hormones',
+    submit: 'Valider',
+    yes: 'Oui',
+    no: 'Non'
+  },
+
+  almostThere: {
+    title: 'Presque là...',
+    profileProcessed: 'Votre profil est en cours de traitement'
+  },
+
+  graph: {
+    myGraph: 'Mon Graph',
+    firstPeriodDay: 'Premier jour de règles',
+    mostRecent: 'Plus recent',
+    endCycle: 'Fin du cycle',
+    sorry: 'Désolé, cette fonctionnalité n\'est pas encore prête.',
+    back: 'Retour'
+  },
+
+  settings: {
+    profil: 'Profil',
+    notifications: 'Notifications',
+    connectSensor: 'Connecter mon capteur'
+  },
+
+  learn: {
+    article: 'Article'
+  }
 }
