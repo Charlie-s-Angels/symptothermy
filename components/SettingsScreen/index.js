387bf5d158ce2f69544263a6d57ec00823d0a9fd
jrkinouani <kinouani@gmail.com>
2020-12-14 14:14:50 +0400

    create button settings

--- components/SettingsScreen/index.js
+++ components/SettingsScreen/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Button, StatusBar, StyleSheet, Alert, } from 'react-native';
+import { View, Button, StatusBar, StyleSheet, Alert, TouchableOpacity, Text} from 'react-native';
 import I18n from '../../i18n/locales';
 
 const SettingsScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <TouchableOpacity  onPress={() => Alert.alert('Button pressed')} style={styles.appButtonContainer}>
+        <Text style={styles.appButtonText}>Profil</Text>
+    </TouchableOpacity>
 
       <Button
         title={I18n.t('settings.profil')}
@@ -26,6 +29,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+    backgroundColor : "#241CC4",
   },
+  appButtonText: {
+    fontSize: 18,
+    color: "gray",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+    backgroundColor: "#fff",
+    width: 200,
+    paddingTop: 60,
+    paddingBottom: 60,
+  }
 });
 export default SettingsScreen;
